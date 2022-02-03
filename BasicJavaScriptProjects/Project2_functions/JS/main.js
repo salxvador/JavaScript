0879042318c0267b1ecbe5dd6c169dc7c04bfe25
salxvador <sal.salvador@gmail.com>
2022-02-03 08:20:32 -0500

    added code comments

--- BasicJavaScriptProjects/Project2_functions/JS/main.js
+++ BasicJavaScriptProjects/Project2_functions/JS/main.js
@@ -1,13 +1,13 @@
 var x = "String 1"
-var y = " String 2"
+var y = " String 2" //Variables for myfunction concatentation
 
 function myFunction() {
     var str = x + y
-    document.getElementById("fn").innerHTML = str;
+    document.getElementById("fn").innerHTML = str; //updated p element when button is clicked.
 }
 
 function pfunc() {
     var sent ="This is ";
-    sent += "my Sentence!";
+    sent += "my Sentence!"; //p text is updated to concatenated strings when clicked. (no button)
     document.getElementById("concat").innerHTML = sent;
 }
