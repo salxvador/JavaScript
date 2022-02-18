4c66d6f6613d0f046dbbf0227ecba9f29df67bc5
salxvador <sal.salvador@gmail.com>
2022-02-18 16:21:32 -0500

    Updated to follow rules of Bingo Columns

--- Bingo/Bingo.js
+++ Bingo/Bingo.js
@@ -1,13 +1,31 @@
 window.onload = initAll;
+var usedNums = new Array(76);
 
 function initAll() {
-    for (var i = 0; i < 24; i++) {
-        setSquare(i);
+    if (document.getElementById) {    
+        for (var i = 0; i < 24; i++) {
+            setSquare(i);
+        }
+    } else {
+        alert("Sorry, your browser does not support this script.");
     }
 }
 
 function setSquare(thisSquare) {
     var currentSquare = "square" + thisSquare;
-    var newNum = Math.floor(Math.random() * 75) + 1;
-    document.getElementById(currentSquare).innerHTML = newNum;
+    var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+    var colBasis = colPlace[thisSquare] * 15;
+    var newNum;
+
+    do {
+        newNum = colBasis + getNewNum() + 1;
+    } 
+    while (usedNums[newNum]);
+
+        usedNums[newNum] = true;
+        document.getElementById(currentSquare).innerHTML = newNum;
+}
+
+function getNewNum() {
+    return Math.floor(Math.random() * 15);
 }
